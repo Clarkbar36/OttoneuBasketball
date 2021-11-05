90499e04096f984412f06d1664bac093cf2ed913
cjromanos92 <cjromanos@gmail.com>
2021-11-04 23:33:47 -0400

    Cody logic update

--- Python_Scripts/main.py
+++ Python_Scripts/main.py
@@ -1,1 +1,15 @@
+#need to build gameCard class?  IGNORE
+gameCard = []
+weeklyDeck = []
+#need to grab list of players from roster  ** ALEX **
 
+
+for  player in roster:
+    # get player name   ** ALEX **
+    # get player team   ** ALEX **
+    # get position      ** ALEX **
+    # get fppg          ** ALEX **
+    # get a list of dates according to player team  ** ALEX **
+    #for each date in schedule where TEAM = 'BOS'
+        #if date > weekStart and date < weekEnd
+    test = 0
