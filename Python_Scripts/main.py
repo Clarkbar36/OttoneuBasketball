95c3b18558af78b2d62d68d5e878e38755ddba57
aclark5 <alexclark36@gmail.com>
2021-11-09 11:16:22 -0500

    update main

---
+++ Python_Scripts/main.py
@@ -0,0 +1,25 @@
+https://pypi.org/project/nbaHeadshots/
+count=0
+for players in weeklyDeck:
+    print(weeklyDeck[count])
+    count+=1
+# G
+guard1_slots =[,,]
+# G
+guard2_slots =[,,]
+# G
+guard3_slots =[,,]
+# F
+forward1_slots =[,,]
+# F
+forward2_slots =[,,]
+# C
+center1_slots =[,,]
+# G/F
+guard_forward_slots=[,,]
+# F/C
+forward_center_slots=[,,]
+# Util
+util_slots=[,,]
+
+from Player_Parsing import pull_roster
